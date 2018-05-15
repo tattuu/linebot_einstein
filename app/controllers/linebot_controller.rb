183062f1365a174829e77d20bd7f309fb0c79df8
Tatsuyoshi Nagao <eeoosu331@yahoo.co.jp>
2018-05-15 04:17:27 +0000

    all

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -36,8 +36,9 @@ class LinebotController < ApplicationController
           tf.binmode
           tf.write(response.body)
           tf.open
-          File.open("#{Rails.root}/public/images/store.jpg","wb") do |file|
+          File.open("#{Rails.root}/public/images/store.jpg","w") do |file|
 #            File.chmod(0777, "#{Rails.root}/public/images/store.jpg")
+            file.binmode
             file.write(tf.read)
             file.close
           end
